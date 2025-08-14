7b66a61748580122ea7ead89fc702c372e313285
abloombe-stack <abloombe@gmail.com>
2025-08-14 01:10:42 -0400

    Setup Environment Variables for PreReno

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
+    'process.env.SUPABASE_ANON_KEY': JSON.stringify(process.env.SUPABASE_ANON_KEY),
+    'process.env.STRIPE_PUBLIC_KEY': JSON.stringify(process.env.STRIPE_PUBLIC_KEY),
+    'process.env.VITE_PUBLIC_URL': JSON.stringify(process.env.VITE_PUBLIC_URL),
+  },
   server: {
     port: 5173,
     host: true
